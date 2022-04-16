857e09aca40d2534dd526460b64352dca43cae1a
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-04-16 13:41:23 -0500

    Adding example 3 (forEach) from 1_operadores.js

---
+++ weekly_mission_2/examples1/example1-3_forEach.js
@@ -0,0 +1,5 @@
+// Ejemplo 3: forEach para imprimir los países en letras mayúsculas
+const countries = ["Finland", "Denmark", "Costa Rica", "Sweden", "Norway", "Iceland", "Mexico"]
+console.log("Ejemplo 5: Imprimiendo la lista de países en mayúsculas")
+
+countries.forEach(element => console.log(element.toUpperCase()))
