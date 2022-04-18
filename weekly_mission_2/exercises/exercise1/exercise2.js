c8294c5fd87fc127c71ff9be41bbefe1c52bab59
Marcos Silva <64155980+MarcosSilvaG@users.noreply.github.com>
2022-04-17 20:42:11 -0500

    Exercise 2 solved

---
+++ weekly_mission_2/exercises/exercise1/exercise2.js
@@ -0,0 +1,21 @@
+const issues = {
+    title: "Live 1 semana 3",
+    repositoryNameAssociated: "MissionNodeJS",
+    status: "Open",
+    numberOfComments: 105,
+    labels: ["-Live 1-", " -Semana 3-"],
+    author: "MarcosSilvaG",
+    dateCreated: "3 apr 2022",
+    lastUpdated: "15 days ago",
+
+    getTitleAndAuthor(){
+        return `Title: ${this.title}\nAuthor: ${this.author}`
+    },
+
+    getGeneralInfo(){
+        return `Title: ${this.title} Labels: ${this.labels}\nComments: ${this.numberOfComments}\nStatus: ${this.status}, Opened ${this.lastUpdated} by ${this.author}`
+    }
+}
+console.log(issues.getTitleAndAuthor())
+console.log("===General info===")
+console.log(issues.getGeneralInfo())
